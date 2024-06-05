2e29e9de3c1d34e29bdde75fc47cdd1d497a47e0
Prashant Solanki <prashantsolanki2025@gmail.com>
2024-06-05 22:04:59 +0530

    Add task Function with ChangeNotifier

--- lib/Modals/task_data.dart
+++ lib/Modals/task_data.dart
@@ -4,13 +4,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:todo_today/Modals/task.dart';
 
 class TaskData extends ChangeNotifier {
-  final List<Task> tasks = [
+  final List<Task> _tasks = [
     Task(taskText: "Buy Milk"),
     Task(taskText: "Buy Bread"),
     Task(taskText: "Buy Butter"),
   ];
 
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(taskText: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
   }
 }
